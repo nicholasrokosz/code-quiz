6ba482c3c174b920ec8c4b6074af3d7b159f0e74
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-11-05 18:01:51 -0700

    added q and a object

---
+++ script.js
@@ -0,0 +1,32 @@
+const qAndA = {
+  "Inside which HTML element do we put the JavaScript?": [
+    "<js>",
+    "<scripting>",
+    "<javascript>",
+    "<script>",
+  ],
+  "Where is the correct place to insert a link to a .js file?": [
+    "The body",
+    "The head",
+    "Either the boday or the head",
+    "Neither the boday nor the head",
+  ],
+  'Which of the following would show the user "Hello World"?': [
+    'alert("Hello World")',
+    'msgBox("Hello World")',
+    'msg("Hello World")',
+    'alerBox("Hello World")',
+  ],
+  "Which of the following defines a function in JavaScript?": [
+    "function=myFunction()",
+    "function myFunction()",
+    "function:myFunction()",
+    "def myFunction()",
+  ],
+  "How can you add a comment in a JavaScript?": [
+    "/* This is a comment */",
+    ";;; This is a comment",
+    "// This is a comment",
+    "# This is a comment",
+  ],
+};
